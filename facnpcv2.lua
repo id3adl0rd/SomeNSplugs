59e70e1fb19138f1262910a9a5d538c219a49bad
D3ADL0RD <raindeadobzor2@gmail.com>
2019-06-19 19:31:07 +0300

    cool

---
+++ facnpcv2.lua
@@ -0,0 +1,49 @@
+PLUGIN.name = "NPC Relations"
+PLUGIN.author = "D3ADL0RD"
+PLUGIN.desc = ":shrug:"
+
+if (SERVER) then
+	local combineNPCClass = {
+		"npc_metropolice",
+		"npc_strider",
+		"npc_combine_s",
+		"npc_helicopter",
+		"npc_manhack",
+		"npc_rollermine",
+		"npc_clawscanner",
+		"npc_turret_ceiling",
+		"npc_combinedropship",
+		"npc_stalker",
+		"npc_combinegunship",
+		"npc_combine_camera",
+		"npc_turret_floor",
+		"npc_cscanner",
+		"npc_turret_ground"
+	}
+	local rebelNPCClass = {
+		"npc_citizen",
+		"npc_alyx",
+		"npc_vortigaunt",
+		"npc_barney"
+	}
+	
+	function PLUGIN:PlayerLoadedChar(client)
+		self:UpdateRelations(client)
+	end
+	
+	function PLUGIN:PlayerSpawnedNPC(client, ent)
+		for k, v in pairs(player.GetAll()) do
+			self:UpdateRelations(v)
+		end
+	end
+	
+	function PLUGIN:UpdateRelations(client)
+		for k, v in pairs( ents.FindByClass( "npc_*" ) ) do
+			if (table.HasValue(combineNPCClass, v:GetClass():lower())) then
+				v:AddEntityRelationship(client, client:isCombine() and D_LI or D_HT)
+			elseif (table.HasValue(rebelNPCClass, v:GetClass():lower())) then
+				v:AddEntityRelationship(client, client:isCombine() and D_HT or D_LI)
+			end
+		end
+	end
+end
