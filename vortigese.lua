1b125cc72063f200f9cd293f634678194b1b3322
cleaneddata <github@sipq.tech>
2019-06-19 20:57:09 +0300

    Update and optimization

--- vortigese.lua
+++ vortigese.lua
@@ -10,17 +10,15 @@ vortigeseWords = {"ahglah", "ahhhr", "alla", "allu", "baah", "beh", "bim", "buu"
 nut.chat.register("vorttrue", {
 	color = Color(114, 175, 237),
 	onChatAdd = function(speaker, text)
+		chat.AddText(Color(114, 175, 237), text)
+	end,
+	onCanHear = function(speaker, listener)
 		local sr = nut.config.get("chatRange", 280)
 		local se = ents.FindInSphere(speaker:GetPos(), sr)
 		for _,v in ipairs(se) do
-			if v:Team() == FACTION_VORT then
-				chat.AddText(Color(114, 175, 237), text)
+			if v:Team == FACTION_VORT then
+				return true
 			end
-		end
-	end,
-	onCanHear = function(speaker, listener)
-		if listener:Team() == FACTION_VORT then
-			return true
 		end
 	end
 })
@@ -37,17 +35,16 @@ nut.chat.register("vortfalse", {
 		end
 
 		local text = table.concat(vort, " ")
-		local sr = nut.config.get("chatRange", 280)
-		local se = ents.FindInSphere(speaker:GetPos(), sr)
-		for k,v in ipairs(se) do
-			if v:Team != FACTION_VORT then
-				chat.AddText(Color(114, 175, 237), text)
-			end
-		end
+
+		chat.AddText(Color(114, 175, 237), text)
 	end,
 	onCanHear = function(speaker, listener)
-		if listener:Team() != FACTION_VORT then
-			return true
+		local sr = nut.config.get("chatRange", 280)
+		local se = ents.FindInSphere(speaker:GetPos(), sr)
+		for _,v in ipairs(se) do
+			if v:Team != FACTION_VORT then
+				return true
+			end
 		end
 	end
 })
@@ -57,14 +54,15 @@ nut.command.add("vort", {
 	onRun = function(client, arguments)
 		local text = table.concat(arguments, " ")
 		local vn = client:Name()
-
-		for _,v in ipairs(player.GetAll()) do
-			local vt = v:Team()
+		if client:Team() == FACTION_VORT then
 			if text:find("%S") then
-				if vt == FACTION_VORT then
-					nut.chat.send(v, "vorttrue", vn.." :"..text)
-				else
-					nut.chat.send(v, "vortfalse", vn.." :"..text)
+				for _,v in ipairs(player.GetAll()) do
+					local vt = v:Team()
+					if vt == FACTION_VORT then
+						nut.chat.send(v, "vorttrue", vn.." :"..text)
+					else
+						nut.chat.send(v, "vortfalse", vn.." :"..text)
+					end
 				end
 			end
 		end
